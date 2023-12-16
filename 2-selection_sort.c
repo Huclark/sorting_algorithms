abc24708fe30ebd591765edb2f9442f5b0115469
Huclark <vhuclark@gmail.com>
2023-12-16 19:11:42 +0000

    Fix: Closed loop before if block

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,42 +1,39 @@
 #include "sort.h"
 
 /**
- * swap_int - swaps two elements in an array
- * @a: the first element
- * @b: the second element
- */
-
-void swap_int(int *a, int *b)
-{
-	int temp;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-/**
- * selection_sort - sort an array of intergers in ascending order
- * uisng the selection sort algorithm
- * @array: the array to be sorted
- * @size: the size of the array
- */
-
+ * selection_sort - Sorts an array of integers in ascending order
+ *                  using the Selection sort algorithm
+ * @array: Array to sort
+ * @size: Size of array to sort
+*/
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_index;
+	int tmp;
+	size_t i, j, k;
 
-	for (i = 0; i < size - 1; i++)
+	if (!array || size < 2)
+		return;
+
+	i = 0;
+	while (i < size - 1)
 	{
-		min_index = i;
+		k = i;
+		j = i + 1;
 
-		for (j = i + 1; j < size; j++)
-			if (array[j] < array[min_index])
-				min_index = j;
-		if (min_index != i)
+		while (j < size)
 		{
-			swap_int(&array[min_index], &array[i]);
+			if (array[j] < array[k])
+				k = j;
+			j++;
+		}
+		if (k != i)
+		{
+			tmp = array[k];
+			array[k] = array[i];
+			array[i] = tmp;
+
 			print_array(array, size);
 		}
+		i++;
 	}
 }
